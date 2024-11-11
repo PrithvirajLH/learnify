6ea82f41f3c75ec6bd65618f287af2a0c4482343
Prithviraj Hulgur <96643920+PrithvirajLH@users.noreply.github.com>
2024-11-10 22:16:51 -0600

    removeCourseCard

--- components/course-cardCourseCard.cy.tsx
+++ components/course-cardCourseCard.cy.tsx
@@ -1,9 +1,1 @@
-import React from 'react'
-import { CourseCard } from './course-card'
 
-describe('<CourseCard />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<CourseCard />)
-  })
-})
